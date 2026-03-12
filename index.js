19332eaca4440912d44b4d1b8fd215ac935eed97
Dennis Schott <>
2026-03-12 07:58:09 +0100

    [ADD] Export: all new modules exported

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import { processRoutes } from './modules/router.js'
 import { mountApp } from './modules/app-mounter.js'
 import { buildRoutes } from './modules/file-router.js'
 
@@ -7,6 +6,55 @@ export { default as Cache } from './modules/cache.js'
 export { configureOwl } from './modules/app-mounter.js'
 export * as Meta from './modules/meta.js'
 export { buildRoutes }
+export { Store, createPersistencePlugin, createStore } from './modules/store.js'
+export {
+  registerLayout,
+  unregisterLayout,
+  getLayout,
+  hasLayout,
+  getLayoutNames,
+  setDefaultLayout,
+  getDefaultLayout,
+  resolveLayout,
+  setRouteLayout,
+  getRouteLayout,
+  createLayoutWrapper,
+  mountWithLayout,
+  getCurrentLayout,
+  subscribeToLayouts,
+  clearLayouts,
+  layout,
+  defineLayout,
+  buildLayouts,
+  discoverLayouts
+} from './modules/layouts.js'
+export {
+  processRoutes,
+  beforeEach,
+  afterEach,
+  getCurrentRoute,
+  getPreviousRoute,
+  isNavigating,
+  cancelNavigation,
+  navigate,
+  push,
+  replace,
+  back,
+  forward,
+  go,
+  router
+} from './modules/router.js'
+export {
+  matchRoute,
+  isDynamicRoute,
+  findRoute,
+  generateUrl,
+  validateRouteParams,
+  createCatchAllRoute,
+  createRedirectRoute,
+  defineRoute,
+  route
+} from './modules/file-router.js'
 
 /**
  * Boots the metaowl application.
